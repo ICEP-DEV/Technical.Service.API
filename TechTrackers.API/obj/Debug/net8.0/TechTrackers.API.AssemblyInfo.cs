a4127d14a83bf3a56d5493dedd16c250673db66b
ezram <ezramagagane@gmail.com>
2024-10-08 17:59:27 +0200

    added a database branch

--- TechTrackers.API/obj/Debug/net8.0/TechTrackers.API.AssemblyInfo.cs
+++ TechTrackers.API/obj/Debug/net8.0/TechTrackers.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechTrackers.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1903dc626f1f2a67c8f8749221f1a6e3de46175")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a0e3886049cadf181ba32f2d96c26b2db4891f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechTrackers.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechTrackers.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TechTrackers.Data/obj/Debug/net8.0/TechTrackers.Data.AssemblyInfo.cs
+++ TechTrackers.Data/obj/Debug/net8.0/TechTrackers.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechTrackers.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1903dc626f1f2a67c8f8749221f1a6e3de46175")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a0e3886049cadf181ba32f2d96c26b2db4891f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechTrackers.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechTrackers.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
