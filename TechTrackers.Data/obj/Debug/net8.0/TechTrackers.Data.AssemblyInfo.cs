df82b8541387592a28128b07d70c9ef610e52df6
nevon <nevondopn@gmail.com>
2024-11-05 03:46:42 +0200

    Nicole

--- TechTrackers.Data/obj/Debug/net8.0/TechTrackers.Data.AssemblyInfo.cs
+++ TechTrackers.Data/obj/Debug/net8.0/TechTrackers.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechTrackers.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01a3bebc0b8830d69c99d9faee66a177d857c95d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60ab4ed0ec82bb2604ffead39134d1c4773e5524")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechTrackers.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechTrackers.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
